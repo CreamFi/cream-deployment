690fe4a89942647dc83a5176ab62ac1083b92d46
Jeremy Yang <jeremy@cream.finance>
2021-10-08 12:06:59 +0800

    Update deploy new market script

--- scripts/deploy_new_market.ts
+++ scripts/deploy_new_market.ts
@@ -1,18 +1,23 @@
 import {deployments, ethers, getNamedAccounts} from 'hardhat';
-const {parseEther, parseUnits} = ethers.utils;
-const {deploy, execute, get, getArtifact} = deployments;
+const {parseUnits} = ethers.utils;
+const {deploy, get, getArtifact, save, run} = deployments;
 
-const crSymbol = 'crWETH.e';
-const crName = 'Cream Wrapped Ether';
-const underlyingAddress = '0x7fCDc2C1EF3e4A0bCC8155a558bB20a7218f2b05';
+enum IRM {
+  Major = 'MajorIRM',
+  Stable = 'StableIRM',
+  Gov = 'GovIRM'
+}
+
+const crSymbol = 'crTESTLINK';
+const crName = 'Cream Chainlink';
+const underlyingAddress = '0x1741B9C475e0861a43B03F984928082Ac4f3fB95';
+const interestRateModel = IRM.Gov;
 
 
 async function main() {
   const {deployer} = await getNamedAccounts();
-  const comptrollerAddress = (await get('Unitroller')).address;
-  const majorIRMAddress = (await get('MajorIRM')).address;
-  const stableIRMAddress = (await get('StableIRM')).address;
-  const govIRMAddress = (await get('GovIRM')).address;
+  const comptrollerAddress = (await get('Comptroller')).address;
+  const irmAddress = (await get(interestRateModel)).address;
   const cTokenAdminAddress = (await get('CTokenAdmin')).address;
   const cTokenImplementationAddress = (await get('CCollateralCapErc20Delegate')).address;
 
@@ -21,13 +26,16 @@ async function main() {
   const underlyingDecimal = await underlying.decimals();
   const initialExchangeRate = parseUnits('0.02', 18 + underlyingDecimal - 8);
 
+  // @ts-ignore
+  // await run("compile:one", { contractName: 'CErc20Delegator' });
+
   const result = await deploy(crSymbol, {
     from: deployer,
     contract: 'CErc20Delegator',
     args: [
       underlyingAddress,
       comptrollerAddress,
-      majorIRMAddress,
+      irmAddress,
       initialExchangeRate,
       crName,
       crSymbol,
